5e46b235c6a19a717742e209a450145cd89f499c
HalaszGergo2 <xlomox12@gmail.com>
2025-01-28 13:39:33 +0100

    api

--- routes/api.php
+++ routes/api.php
@@ -27,7 +27,7 @@ Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::patch('update-profile', [UserController::class, 'updateProfile']);
+Route::put('update-profile', [UserController::class, 'updateProfile']);
 
 
 Route::middleware(['auth:sanctum', User::class])
