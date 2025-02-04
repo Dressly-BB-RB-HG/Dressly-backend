865f17e09bc562e6479a2e553f20109245cb6797
HalaszGergo2 <xlomox12@gmail.com>
2025-02-04 09:47:30 +0100

    teszt maszek

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -25,6 +25,8 @@ class UserFactory extends Factory
     {
         return [
             'name' => fake()->name(),
+            'k_nev' => fake()->name(),
+            'v_nev' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -14,6 +14,8 @@ class RegistrationTest extends TestCase
         $response = $this->post('/register', [
             'name' => 'Test User',
             'email' => 'test@example.com',
+            'k_nev' => 'teszt',
+            'v_nev' => 'teszt',
             'password' => 'password',
             'password_confirmation' => 'password',
         ]);
