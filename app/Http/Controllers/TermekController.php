66581605555e1145616b8dcdd2f318cbc55456da
HalaszGergo2 <xlomox12@gmail.com>
2025-02-04 10:59:50 +0100

    meret lekerdezese

--- app/Http/Controllers/TermekController.php
+++ app/Http/Controllers/TermekController.php
@@ -101,23 +101,19 @@ class TermekController extends Controller
 
         return response()->json($termekek);
     }
-
- 
-
-
-    public function rovidUjjuPolok()
-    {
-        $termekek = Termek::with(['modell.kategoria'])
-            ->whereHas('modell.kategoria', function ($query) {
-                $query->where('ruhazat_kat', 'Rövid ujjú póló');
-            })
-            ->get();
-}
     
     public function kategoriaRuhak(string $kategoria){
         $termekek = Termek::join('modells', 'termeks.modell', '=', 'modells.modell_id')
         ->join('kategorias', 'modells.kategoria', '=', 'kategorias.kategoria_id')
         ->where('kategorias.ruhazat_kat', $kategoria) 
+        ->get(['termeks.*']);
+
+        return response()->json($termekek);
+    }
+
+    public function meretRuhak(string $meret){
+        $termekek = Termek::join('modells', 'termeks.modell', '=', 'modells.modell_id')
+        ->where('termeks.meret', $meret)
         ->get(['termeks.*']);
 
         return response()->json($termekek);
